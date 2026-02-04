34e5041a0440ff75ff67bebdc397b0bfd01f61ec
jy4education_git <jy4education@gmail.com>
2026-02-04 12:06:38 +0100

    +

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,23 +4,24 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		String str;  // String
+		String str;  
 		char ch; // Character
 		
 		try 
 		{		
-			str = "Irgend eine Zeichenkette";
-			System.out.println("Stringlaenge :" + str.length());
+			str = null;  // String Errpr -> str = null NullPointer Exception
+			System.out.println("Stringlänge :" + str.length());
 			
-			ch = str.charAt(0);
+			ch = str.charAt(0); // Error -> str.length()
 			System.out.println("Character :" + ch);	
 		} 
 		catch (StringIndexOutOfBoundsException e) {
 			// Fehlerbehandlung ...
-			System.out.println("Index Fehler, bitte Eingabe pruefen!");		
+			System.out.println("Index Fehler, bitte Eingabe prüfen!");		
 		}
 		catch (Exception e) {
 			// Weitere Fehler-Routinen ....
+			System.out.println("Uiuiuiuiuiu!!!  Bitte Eingabe dringend prüfen!");
 		}
 		
 		
